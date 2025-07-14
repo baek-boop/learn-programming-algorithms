8d45daef3e088e8e71b99507c45ad252b08afffb
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-07-15 01:22:22 +0700

    11_07_2025

--- Java/july_2025/12_07_2025/avatar.java
+++ Java/july_2025/12_07_2025/avatar.java
@@ -3,5 +3,10 @@ import java.util.Scanner;
 public class avatar {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.printf("Program Avatar\n");
+        System.out.printf("Masukkan nama Anda: ");
+
+
     }
 }
