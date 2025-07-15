073b17fc1462a9e187930f3482013ca83f21d539
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-07-16 02:01:08 +0700

    tes

--- Java/july_2025/12_07_2025/tes.java
+++ Java/july_2025/12_07_2025/tes.java
@@ -1,5 +1,11 @@
 public class tes{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(
+            "This is a test for the Java file in the 12_07_2025 directory."
+        System.out.println("This file is created to test the directory structure and file creation."
+        System.out.println("It should compile and run without any issues."
+
+        );
     }
 }
