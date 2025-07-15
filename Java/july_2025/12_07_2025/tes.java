a4123d8c780eb5f46a2a5a77638b8bf980653071
Bintang Ridwan Pribadi <114246987+bintangridwanp@users.noreply.github.com>
2025-07-16 01:52:34 +0700

    tes

--- Java/july_2025/12_07_2025/tes.java
+++ Java/july_2025/12_07_2025/tes.java
@@ -1,13 +1,5 @@
-import org.w3c.dom.ls.LSOutput;
-
-public class tes {
+public class tes{
     public static void main(String[] args) {
-        System.out.println("tes");
+        System.out.println("Hello, World!");
     }
-    LSOutput
-
-        sads
-avatara
-    avataraa
-        a
 }
