11547af2f0de1e150cdc2bc8c20aef03904190ad
Bintang Ridwan Pribadi <bintangridwan30@gmail.com>
2025-07-16 02:34:41 +0700

    memperbaiki file final

--- Java/july_2025/12_07_2025/avatar.java
+++ Java/july_2025/12_07_2025/avatar.java
@@ -5,9 +5,5 @@ public class avatar {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.printf("Program Avatar\n");
-        System.out.printf("Masukkan nama Anda: ");
-
-        DoubleToIntFunction
     }
 }
